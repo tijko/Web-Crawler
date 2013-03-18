3f83fb18e345fcdef8d1d6122b4f7b4fea93311b
rhea <rhea@rhea.(none)>
2013-03-18 07:59:16 -0400

    edit

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ def options():
     parser.add_argument('-c', '--count', help='Get a count on the most frequently used words', action='store_true')
     parser.add_argument('-l', '--limit', help='Set a limit on pages crawled', action='store_true')
     parser.add_argument('-u', '--url', help='Limit search to pages with a specific "word" in the url', action='store_true')
+    parser.add_argument('-m', '--minus', help='Skip urls with a specific "word"', action='store_true')
     parse = parser.parse_args()
     return parse
 
 
 def main(options):
     word = None
+    url = None
+    skip = None
     count = False
     limit = 100
-    url = None
     site = raw_input('Enter valid url to begin the crawl: ')
     if options.word:
         word = raw_input('Enter a word to search: ')
@@ -32,7 +34,13 @@ def main(options):
                 pass
     if options.url:
         url = raw_input('What word do you want to set the URL\'s to?: ')
-    Spider(site, word=word, count=count, limit=limit, url=url)
+    if options.minus:
+        skip = raw_input('What word do you want to leave out of URL\'s?: ')
+    if url and skip:
+        if url == skip:
+            print 'Can\'t skip and include, the same word.'
+            return
+    Spider(site, word=word, count=count, limit=limit, url=url, skip=skip)
     return
             
 
