6bdca02c548fa1ec30175880c49605d75708a0ef
rhea <rhea@rhea.(none)>
2013-03-18 07:59:45 -0400

    edit

--- lib/webcrawl.py
+++ lib/webcrawl.py
@@ -11,10 +11,11 @@ class Spider(object):
     def __init__(self, site, **kwargs):
         self.site = site
         wrdpg = []
+        urls = []
         try:
             data, soup = self.crawl(self.site)
         except TypeError:
-            print 'Bad Url -- check the address to make sure its correct'
+            print 'Bad Url -- check the address to make sure its correct(include http://)'
             return
         if not data:
             print 'Bad Url -- check the address to make sure its correct'
@@ -22,11 +23,23 @@ class Spider(object):
         if kwargs['word'] and kwargs['word'] in data:
             wrdpg.append(self.site)
         if kwargs['url']:
-            urls = [i.get('href') for i in soup.findAll('a') if kwargs['url'] 
-                    in str(i.get('href')) and 'http' in str(i.get('href'))] 
-        else:
-            urls = [i.get('href') for i in soup.findAll('a') if 'http' in
-                    str(i.get('href'))]
+            urls = [i.get('href') for i in soup.findAll('a')
+                    if i.get('href') is not None and 
+                    kwargs['url'] in i.get('href') and 
+                    'http' in i.get('href')] 
+        if kwargs['skip']:
+            if urls:
+                urls = [i for i in urls if kwargs['skip'] not in i]
+            else:
+                urls = [i.get('href').encode('utf-8') for i in soup.findAll('a') 
+                        if i.get('href') is not None and 
+                        'http' in i.get('href') and
+                        kwargs['skip'] in i.get('href')]
+        if not kwargs['url'] and not kwargs['skip']:
+            urls = [i.get('href') for i in soup.findAll('a') 
+                    if i.get('href') is not None and
+                    'http' in i.get('href')]
+        urls = [i for i in set(urls)]
         if len(urls) >= kwargs['limit']:
             if not kwargs['word'] and not kwargs['count']:
                 for i in urls[:kwargs['limit']]:
@@ -34,12 +47,12 @@ class Spider(object):
             else:
                 for i in urls[:kwargs['limit']]:
                     try:
-                        new_data, soup = self.crawl(str(i))
+                        new_data, soup = self.crawl(i)
                         if new_data:
                             if kwargs['word'] and kwargs['word'] in new_data:
                                 wrdpg.append(i)
                             data = data + new_data
-                    except (TypeError, UnicodeEncodeError) as e:
+                    except TypeError:
                         pass 
             if kwargs['word']:
                 for i in urls[:kwargs['limit']]:
@@ -57,12 +70,20 @@ class Spider(object):
                     new_data, soup = self.crawl(urls[pos])
                     if soup:
                         if kwargs['url']:
-                            urls = urls + [i.get('href') for i in soup.findAll('a')
-                                           if kwargs['url'] in str(i.get('href'))
-                                           and 'http' in str(i.get('href'))]
-                        else:
-                            urls = urls + [i.get('href') for i in soup.findAll('a')
-                                           if 'http' in str(i.get('href'))]
+                            urls = urls + [i.get('href').encode('utf-8') for i in soup.findAll('a')
+                                           if i.get('href') is not None and
+                                           kwargs['url'] in i.get('href')
+                                           and 'http' in i.get('href')]
+                        if kwargs['skip']:
+                            urls = [i for i in urls if kwargs['skip'] not in i]
+                            urls = urls + [i.get('href').encode('utf-8') for i in soup.findAll('a')
+                                           if i.get('href') is not None and
+                                           kwargs['skip'] not in i.get('href') and
+                                           'http' in i.get('href')]
+                        if not kwargs['url'] and kwargs['skip']:
+                            urls = urls + [i.get('href').encode('utf-8') for i in soup.findAll('a')
+                                           if i.get('href') is not None and
+                                           'http' in i.get('href')]
                     if new_data:
                         if kwargs['word'] and kwargs['word'] in new_data:
                             wrdpg.append(urls[pos])
@@ -70,8 +91,13 @@ class Spider(object):
                 except TypeError:
                     pass
                 except IndexError:
+                    if urls:
+                        for i in urls:
+                            print i
+                        return
                     print 'Sorry no Url\'s'
                     return
+                urls = [i for i in set(urls)]
                 pos += 1
         if not kwargs['word'] and not kwargs['count']:
             for i in urls[:kwargs['limit']]:
@@ -85,8 +111,6 @@ class Spider(object):
         if kwargs['count']:
             print Counter(data)
         return
-
-
 
 
     def crawl(self, page):
